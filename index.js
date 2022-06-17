bf4bbf65af0a22aee4793223e1a4cc138bc3249d
Collins Mundia <collins.mwangi@student.moringaschool.com>
2022-06-17 05:01:11 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(argument='roller-skate'){
+    return `This Saturday, I want to ${argument}!`;
+}
+
+
+let mondayWork = function(argument="go to the office") {
+    return `This Monday, I will ${argument}.`
+  }
+
+  let wrapAdjective = function(wr="*") {
+    return function(adj="special") {
+      return `You are ${wr}${adj}${wr}!`
+    }
+  }
